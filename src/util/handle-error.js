03cd1288992bbb97976fe055ce09851e13d93466
火光 <huoguang@huoguangdeMacBook-Pro.local>
2023-10-06 16:49:11 +0800

    删除冗余注释

--- src/util/handle-error.js
+++ src/util/handle-error.js
@@ -1,11 +1,7 @@
 /**
- * Copyright (c) Baidu Inc. All rights reserved.
- *
- * This source code is licensed under the MIT license.
- * See LICENSE file in the project root for license information.
- *
- * 处理组件异常
+ * @file 处理组件异常
  */
+
 function handleError(e, component, info) {
     var current = component;
 
